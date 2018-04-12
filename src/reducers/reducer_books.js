1f3813d3013b098a35729996cdace18533601054
Brunner Ursin <ursin.brunner@ti8m.ch>
2018-04-12 21:06:08 +0200

    add some event stuff

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,7 @@
+export default () => {
+    return [
+        {title: 'Javascript: the Good Parts', pages: 23},
+        {title: 'Harry Potter', pages: 33},
+        {title: 'Whatever Book', pages: 453}
+    ]
+}
