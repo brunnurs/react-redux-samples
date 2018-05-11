6681377f6277d2d74c95734d43449f91211d562b
Brunner Ursin <ursin.brunner@ti8m.ch>
2018-05-11 08:31:58 +0200

    ongoing process

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {BrowserRouter, Route} from 'react-router-dom';
+import promise from 'redux-promise';
 
-import App from './components/app';
+import PostInex from './components/post_index';
+
+
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <Route path="/" component={PostInex}/>
+            </div>
+        </BrowserRouter>
+    </Provider>
+    , document.querySelector('.container'));
